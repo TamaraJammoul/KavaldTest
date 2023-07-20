62d8e3c906749277b82aaae4f3c130181af2c92b
tamarajammoul <tamara.jamoul@phinexes.com>
2023-07-21 02:58:38 +0400

    add lint command

--- src/app/components/books-management/components/books-list/books-list.component.ts
+++ src/app/components/books-management/components/books-list/books-list.component.ts
@@ -33,18 +33,14 @@ import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-
 })
 export class BooksListComponent extends BaseComponent implements OnInit {
   @ViewChild('bookInput') bookInput: ElementRef<HTMLInputElement>;
-  @Input({ required: true })
-  set booksList(list: BooksList) {
-    this.books = list.books;
-    this.booksListsItem = list;
-  }
+  @Input({ required: true }) booksList: BooksList;
+
 
   bookFormControl = new FormControl();
   separatorKeysCodes: number[] = [ENTER, COMMA];
   filteredBooks$: Observable<Book[]>;
   availableBooks: Book[] = [];
-  books: Book[];
-  booksListsItem: BooksList;
+
 
 
   constructor(private bookService: BookService) { super(); }
@@ -60,7 +56,7 @@ export class BooksListComponent extends BaseComponent implements OnInit {
     this.filteredBooks$ = this.bookFormControl.valueChanges.pipe(
       startWith(null),
       map((value: string | null) => (value ? this._filter(value) :
-        this.availableBooks.filter(book => this.books.findIndex(e => e.id === book.id) === -1).slice())),
+        this.availableBooks.filter(book => this.booksList.books.findIndex(e => e.id === book.id) === -1).slice())),
     );
 
   }
@@ -69,34 +65,34 @@ export class BooksListComponent extends BaseComponent implements OnInit {
   onBookSelected(event: MatAutocompleteSelectedEvent): void {
     const bookId = event.option.value;
     const book = this.availableBooks.find(e => e.id === bookId);
-    this.booksListsItem.books.push(book);
+    this.booksList.books.push(book);
     this.bookInput.nativeElement.value = '';
     this.bookFormControl.setValue(null);
-    this.bookService.updateBooksList({ ...this.booksListsItem });
+    this.bookService.updateBooksList({ ...this.booksList });
   }
 
   remove(bookId: string): void {
-    const index = this.booksListsItem.books.findIndex(e => e.id === bookId);
+    const index = this.booksList.books.findIndex(e => e.id === bookId);
 
     if (index >= 0) {
-      this.booksListsItem.books.splice(index, 1);
-      this.bookService.updateBooksList({ ...this.booksListsItem });
+      this.booksList.books.splice(index, 1);
+      this.bookService.updateBooksList({ ...this.booksList });
     }
   }
 
   private _filter(value: string): Book[] {
     const filterValue = value.toLowerCase();
-    const data = this.availableBooks.filter(book => this.books.findIndex(e => e.id === book.id) === -1);
+    const data = this.availableBooks.filter(book => this.booksList.books.findIndex(e => e.id === book.id) === -1);
     return data.filter(book => book.title.toLowerCase().includes(filterValue));
   }
 
   onListDeleted() {
-    this.bookService.deleteBooksList(this.booksListsItem);
+    this.bookService.deleteBooksList(this.booksList);
   }
 
   drop(event: CdkDragDrop<unknown>) {
-    moveItemInArray(this.books, event.previousIndex, event.currentIndex);
-    this.bookService.updateBooksList({ ...this.booksListsItem, books: this.books });
+    moveItemInArray(this.booksList.books, event.previousIndex, event.currentIndex);
+    this.bookService.updateBooksList({ ...this.booksList });
   }
 
 }
